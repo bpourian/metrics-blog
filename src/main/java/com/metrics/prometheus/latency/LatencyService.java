e8332cd9a165343768c08ae44880b19612d99074
Benjamin Pourian <benjamin.pourian@gmail.com>
2019-06-16 17:18:01 +0100

    Add unit test for latency

--- src/main/java/com/metrics/prometheus/latency/LatencyService.java
+++ src/main/java/com/metrics/prometheus/latency/LatencyService.java
@@ -1,20 +1,59 @@
 package com.metrics.prometheus.latency;
 
 import com.google.inject.Singleton;
+import io.prometheus.client.Collector;
+import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.Histogram;
 
 @Singleton
 public class LatencyService {
 
-  private final Histogram latencyHistogram = Histogram.build()
-      .name("api_request_latency_millis")
-      .help("Request latency in milliseconds")
-      .labelNames("method", "endpoint")
-      .buckets(0.5D, 1D, 2D, 3D, 4D, 5D)
-      .register();
+  private final Histogram latencyHistogram;
+  private final CollectorRegistry registry;
+
+  public static final String API_REQUEST_LATENCY_MILLIS = "api_request_latency_millis";
+  public static final String REQUEST_LATENCY_IN_MILLISECONDS = "Request latency in milliseconds";
+  public static final String METHOD = "method";
+  public static final String ENDPOINT = "endpoint";
 
 
-  public Histogram.Timer startTimerWithLabelValues(MethodValues method, String endPoint) {
-    return this.latencyHistogram.labels(method.toString(), endPoint).startTimer();
+  public LatencyService() {
+
+    this.registry = new CollectorRegistry();
+
+    this.latencyHistogram = Histogram.build()
+        .name(API_REQUEST_LATENCY_MILLIS)
+        .help(REQUEST_LATENCY_IN_MILLISECONDS)
+        .labelNames(METHOD)
+        .buckets(0.25D, 0.5D, 1D, 2D)
+        .register(registry);
+
+  }
+
+
+  public Histogram.Timer startTimerWithLabelValues(MethodValues method) {
+    return this.latencyHistogram.labels(method.toString()).startTimer();
+  }
+
+  public Histogram getLatencyHistogram() {
+    return latencyHistogram;
+  }
+
+  public CollectorRegistry getRegistry() {
+    return registry;
+  }
+
+  public double getCount(MethodValues method) {
+    return registry.getSampleValue(method.toString());
+  }
+
+  public double getSum(MethodValues method) {
+    return registry.getSampleValue(method.toString());
+  }
+
+  public double getBucket(double b, String labelName) {
+    return registry.getSampleValue(labelName,
+        new String[]{"le"},
+        new String[]{Collector.doubleToGoString(b)});
   }
 }
--- src/test/java/com/metrics/prometheus/latency/LatencyServiceTest.java
+++ src/test/java/com/metrics/prometheus/latency/LatencyServiceTest.java
@@ -1,13 +1,19 @@
 package com.metrics.prometheus.latency;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.prometheus.client.Collector;
+import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.Histogram;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LatencyServiceTest {
 
-  // under test
   private LatencyService underTest;
+  private CollectorRegistry registry;
 
   @BeforeEach
   void setUp() {
@@ -17,25 +23,52 @@ class LatencyServiceTest {
   @Test
   void buildTimer() {
     // Given
-    Histogram.Timer getMethodTimer = underTest
-        .startTimerWithLabelValues(MethodValues.GET, "http://localhost:9090/api/v1/happy");
-
-    Histogram.Timer postMethodTimer = underTest
-        .startTimerWithLabelValues(MethodValues.POST, "http://localhost:9090/api/v1/happy");
+    Histogram.Timer getMethodTimer = underTest.startTimerWithLabelValues(MethodValues.GET);
+    Histogram.Timer postMethodTimer = underTest.startTimerWithLabelValues(MethodValues.POST);
 
     // When
-    try {
-      Thread.sleep(2000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    } finally {
-      getMethodTimer.observeDuration();
-      postMethodTimer.observeDuration();
-    }
+
+    List<Integer> sleep = Arrays.asList(200, 450, 900, 1800, 3000);
+
+    sleep.forEach(milliseconds -> {
+
+      try {
+        Thread.sleep(milliseconds);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      } finally {
+        getMethodTimer.observeDuration();
+        postMethodTimer.observeDuration();
+      }
+    });
 
     // Then
+
+    underTest.getRegistry();
+//    System.out.println(underTest.getCount(MethodValues.GET));
+//    System.out.println(underTest.getCount(MethodValues.POST));
+
     System.out.println(getMethodTimer);
     System.out.println(postMethodTimer);
-    System.out.println(underTest);
+    System.out.println(underTest.getLatencyHistogram());
+    assertThat(underTest.getLatencyHistogram().collect().get(0)).isEqualTo(null);
+  }
+
+  public CollectorRegistry getRegistry() {
+    return registry;
+  }
+
+  public double getCount(MethodValues method) {
+    return registry.getSampleValue(method.toString());
+  }
+
+  public double getSum(MethodValues method) {
+    return registry.getSampleValue(method.toString());
+  }
+
+  public double getBucket(double b, String labelName) {
+    return registry.getSampleValue(labelName,
+        new String[]{"le"},
+        new String[]{Collector.doubleToGoString(b)});
   }
 }
